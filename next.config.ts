5f81bb9bbd9e29155db08185825f8f438aa83ff3
AliZiegler <ali12xx66@gmail.com>
2025-06-29 12:10:16 +0300

    made wrangler.toml and changed a bit of next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    experimental: {
+        serverComponentsExternalPackages: ['@cloudflare/next-on-pages']
+    }
 };
 
 export default nextConfig;
