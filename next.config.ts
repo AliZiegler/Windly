8667f53035aa2c4a7608bf8dfd325b33776d10cc
AliZiegler <ali12xx66@gmail.com>
2025-06-29 12:31:21 +0300

    removed serverExternalPackage from nextjs config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    experimental: {
-        serverExternalPackages: ['@cloudflare/next-on-pages']
-    }
 };
 
 export default nextConfig;
