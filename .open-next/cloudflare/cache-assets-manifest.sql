2485ae93c2c6df050e0bf51df03c0d4150696785
AliZiegler <ali12xx66@gmail.com>
2025-07-26 12:25:14 +0300

    push

--- .open-next/cloudflare/cache-assets-manifest.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS tags (tag TEXT NOT NULL, path TEXT NOT NULL, UNIQUE(tag, path) ON CONFLICT REPLACE);
-     CREATE TABLE IF NOT EXISTS revalidations (tag TEXT NOT NULL, revalidatedAt INTEGER NOT NULL, UNIQUE(tag) ON CONFLICT REPLACE);
-INSERT INTO tags (tag, path) VALUES ("zzrECa9h34Sc7Bm39ot_E/_N_T_/layout", "zzrECa9h34Sc7Bm39ot_E/icon.png"), ("zzrECa9h34Sc7Bm39ot_E/_N_T_/icon.png/layout", "zzrECa9h34Sc7Bm39ot_E/icon.png"), ("zzrECa9h34Sc7Bm39ot_E/_N_T_/icon.png/route", "zzrECa9h34Sc7Bm39ot_E/icon.png"), ("zzrECa9h34Sc7Bm39ot_E/_N_T_/icon.png", "zzrECa9h34Sc7Bm39ot_E/icon.png");
